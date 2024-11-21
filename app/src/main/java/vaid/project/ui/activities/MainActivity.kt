ef3b0305e96f5ae961b9838f16e921b1de26a89c
Vaid <mode_private@mail.ru>
2024-11-21 22:37:15 +0400

    Add menu to toolbar and bottom navigation view with navigation graph

--- app/src/main/java/vaid/project/ui/activities/MainActivity.kt
+++ app/src/main/java/vaid/project/ui/activities/MainActivity.kt
@@ -2,11 +2,32 @@ package vaid.project.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import vaid.project.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var toolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navigationController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navigationController)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
