e99ed918b94c94fce3bdd59d7cd8ea275f3b758b
Vaid <mode_private@mail.ru>
2024-11-21 17:30:39 +0400

    Create fragments for main activity

--- app/src/main/java/vaid/project/ui/activities/MainActivity.kt
+++ app/src/main/java/vaid/project/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package vaid.project
+package vaid.project.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import vaid.project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
